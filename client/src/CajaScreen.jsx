2410f1ba634c1ed24722b8d928b6b4d8a3517090
Ariel Fernandez <90484008+AriDsec@users.noreply.github.com>
2026-03-25 19:39:26 -0600

    Update CajaScreen.jsx

--- client/src/CajaScreen.jsx
+++ client/src/CajaScreen.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Header, payBadge, BillModal, ItemsModal, SplitModal, Spinner, imprimirTiquete } from './components.jsx';
+import { Header, payBadge, imprimirTiquete, Spinner } from './ui.jsx';
+import { BillModal, ItemsModal, SplitModal } from './modals.jsx';
 
 function CajaStats({ paid, zona }) {
   const totalCobrado = paid.reduce((s, o) => s + o.total, 0);
