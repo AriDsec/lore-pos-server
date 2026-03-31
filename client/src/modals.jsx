9b4ccf8697ed8d2f0fdcb7d7a3ab654d76d73114
Ariel Fernandez <90484008+AriDsec@users.noreply.github.com>
2026-03-30 20:33:40 -0600

    Actualizar modals.jsx

--- client/src/modals.jsx
+++ client/src/modals.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { PINES, meseras as MESERAS } from './constants.js';
 import { payBadge, imprimirTiquete } from './ui.jsx';
 
